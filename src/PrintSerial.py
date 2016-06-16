0e989016f717077683277960998a7d7e52e228a1
kr0 <kr0@github.com>
2016-06-16 17:23:19 -0400

    [kr0] print serial automatically looks for port connected by usb.

--- src/PrintSerial.py
+++ src/PrintSerial.py
@@ -1,11 +1,26 @@
 import serial
+import serial.tools.list_ports
 
-with serial.Serial() as ser:
-    ser.baudrate = 9600
-    ser.port = '/dev/cu.usbmodem1411'
-    ser.open()
+def serial_ports():
+    ports = list(serial.tools.list_ports.comports())
+
+    for port_no, description, address in ports:
+        if 'USB' in address:
+            return port_no
+
+
+def print_serial():
+    with serial.Serial() as ser:
+        ser.baudrate = 9600
+        ser.port = serial_ports()
+        ser.open()
     
-    while(True):
-        if (ser.inWaiting() > 0):
-            myData = ser.readline()
-            print myData
+        while(True):
+            if (ser.inWaiting() > 0):
+                myData = ser.readline()
+                print myData
+
+
+
+if __name__ == "__main__":
+    print_serial()
